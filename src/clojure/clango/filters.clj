5dd6d27eb4de46b63ff13725944d8f3a4c7cc487
Jack Moffitt <jack@metajack.im>
2011-10-08 17:23:38 -0600

    Added first, last, and join filters.

--- src/clojure/clango/filters.clj
+++ src/clojure/clango/filters.clj
@@ -1,4 +1,19 @@
-(ns clango.filters)
+(ns clango.filters
+  (:refer-clojure :exclude [first last])
+  (:require [clojure.core :as core]
+            [clojure.string :as str]))
+
+(defn first [x]
+  (core/first x))
+
+(defn join
+  ([x]
+     (str/join x))
+  ([x s]
+     (str/join s x)))
+
+(defn last [x]
+  (core/last x))
 
 (defn length [x]
   (count x))
