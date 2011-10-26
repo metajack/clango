90e80ca6003e3a36a915fb84cfc4c02a68ba4194
Jack Moffitt <jack@metajack.im>
2011-10-25 23:16:50 -0600

    Added file and map stores.

---
+++ src/clojure/clango/stores.clj
@@ -0,0 +1,19 @@
+(ns clango.stores
+  (:require [clojure.string :as str])
+  (:import [java.io File IOException]))
+
+(defn- path-join [& paths]
+  (str/join File/separator paths))
+
+(defn file-store
+  ([]
+     (file-store (System/getProperty "user.dir")))
+  ([root]
+     (fn [name]
+       (try
+         (slurp (path-join root name))
+         (catch IOException e nil)))))
+
+(defn map-store [template-map]
+  (fn [name]
+    (find template-map name)))
