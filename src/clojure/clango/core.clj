ba8125995754ae0095403881f0ca5d80756eeb54
Jack Moffitt <jack@metajack.im>
2012-01-14 00:24:22 -0700

    Add render-template and render-string public APIs.

--- src/clojure/clango/core.clj
+++ src/clojure/clango/core.clj
@@ -12,22 +12,22 @@
 
 (def ^:dynamic *context* {})
 
-(defn- render-raw [[text] stack _]
+(defn- render-part-raw [[text] stack _]
   [text stack])
 
-(defn- render-var [v stack context]
+(defn- render-part-var [v stack context]
   [(str (var/process-var (cons :var v) context)) stack])
 
-(defn- render-block [[name & parts] stack context]
+(defn- render-part-block [[name & parts] stack context]
   (let [tag (tags/tag-for-name :render name)]
     (tag context parts stack)))
 
-(defn- render-template [parts stack context]
+(defn- render-part-template [parts stack context]
   (let [parts (tags/compile parts)]
     ["" (concat parts stack)]))
 
 (defn- render-part [[type & parts] stack context]
-  (let [f @(ns-resolve my-ns (symbol (str "render-" (name type))))]
+  (let [f @(ns-resolve my-ns (symbol (str "render-part-" (name type))))]
     (f parts stack context)))
 
 (defn render [[_ & parts] & {:keys [context] :or {context *context*}}]
@@ -42,3 +42,14 @@
             (let [[res new-stack] (render-part part next-parts ctx)]
               (recur new-stack ctx (conj acc res)))))
         (apply str acc)))))
+
+(defn render-template [name & {:keys [context store] :or {context *context*}}]
+  (let [data (store name)
+        template (parser/parse data)
+        context (assoc context ::template-store store)]
+    (render template :context context)))
+
+(defn render-string [s & {:keys [context store] :or {context *context*}}]
+  (let [template (parser/parse s)
+        context (assoc context ::template-store store)]
+    (render template :context context)))
