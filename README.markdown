769802e7c440fc8a41f12214a451e1043f5c4ae6
Jack Moffitt <jack@metajack.im>
2012-06-17 20:34:49 -0600

    Add license information.

--- README.markdown
+++ README.markdown
@@ -1,4 +1,8 @@
 # clango
 
-Templating system for Clojure compatible with Django templates.
+Templating system for Clojure similiar to Django templates.
 
+## License
+
+Clango is licensed under the EPL v1.0. See the file epl-v10.html for
+details.
--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject clango "1.0.0-SNAPSHOT"
   :description "Django-style templates for Clojure"
+
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [enlive "1.0.0"]
                  [org.antlr/antlr "3.4"]]
