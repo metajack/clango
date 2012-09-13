7869902452a394faa59362e4916f70f951f7bdc2
Jack Moffitt <jack@metajack.im>
2012-09-13 06:58:26 -0600

    Update enlive dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :distribution :repo}
   
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [enlive "1.0.0"]
+                 [enlive "1.0.1"]
                  [org.antlr/antlr "3.4"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"])
